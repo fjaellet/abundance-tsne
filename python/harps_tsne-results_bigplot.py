948d756535cec1bbdf9b7469e003caffd55f9bc2
friedrich <fanders@aip.de>
2017-11-14 19:01:44 +0100

    more coding discipline!

--- python/harps_tsne-results_bigplot.py
+++ python/harps_tsne-results_bigplot.py
@@ -15,102 +15,78 @@ from astropy.io import fits as pyfits
 from astroML.plotting import setup_text_plots
 setup_text_plots(fontsize=10, usetex=True)
 
-sets = "errlim" # either 'mc', 'errlim' or 'plain'
+import open_data
+
+sets = "errlim"                # either 'mc', 'errlim' or 'plain'
   
-hdu = pyfits.open(
-    '/home/friedel/Astro/Spectro/HARPS/DelgadoMena2017.fits',
-    names=True)
-data=hdu[1].data
+t     = open_data.harps()
+data  = t.data
+t.get_tsne_subsets( sets = sets )
 
 labels = [r'$\rm \tau \ [Gyr]$', r'$\rm [Fe/H]$', r'$\rm [Ti/Fe]$',
           r'$\rm [Y/Mg]$', r'$\rm [Zr/Fe]$', r'$\rm [Al/Mg]$']
 
-xx  = [data['meanage'], data['feh'], data['TiIFe'], data['YFe']-data['MgFe'],
-          data['ZrIIFe'], data['AlFe']-data['MgFe']]
+xx  = [data['meanage'], data['feh'],
+       data['TiIFe'], data['YFe']-data['MgFe'],
+       data['ZrIIFe'], data['AlFe']-data['MgFe'],
+       data['MgFe'], data['Ulsr'],
+       data['Vlsr'], data['Wlsr'], 
+       data['Teff'], data['logg_hip'], 
+       data['R_2'], data['Z'], 
+       data['X'], data['Y'], 
+       data['BaFe'], data['YFe']-data['AlFe'],
+       data['NdFe'], data['SrFe']-data['BaFe'],
+       data['CeFe'], data['ZrFe']-data['BaFe'],
+       data['ZnFe'], data['EuFe'],
+       data['CuFe'], data['[O/H]_6158_BdL15']-data['feh'],
+       data['[C/H]_SA17']-data['feh'], data['[C/H]_SA17']-data['[O/H]_6158_BdL15'] ]
+
 xerr = [data['agestd'], data['erfeh'], data['errTiI'],
         np.sqrt(data['errY']**2.+data['errMg']**2.), data['errZrII'],
         np.sqrt(data['errAl']**2.+data['errMg']**2.) ]
 
-if sets=="mc":
-    Xt = data["X_tsne_teffcut40_mc"]
-    Yt = data["Y_tsne_teffcut40_mc"]
-    classcol= np.char.rstrip(data["tSNE_class_mc"],' ')
-    subsets = ["thin", "thick1", "thick2", "thick3", "thick4",
-               "mpthin", "smr", "t4trans",
-               "debris1", "debris2", "debris3", "debris4", "debris5?", 
-               "t2trans1", "t2trans2", "highTi","thicklow"]
-    names   = ["Thin Disc", "Thick Disc I", "Thick Disc II", "Thick Disc III",
-               "Thick Disc IV", "Metal-poor \n Thin Disc", "SMR", "Transition",
-               "", "", "Satellite \n debris", "", "", r"TII/III", "", 
-               r"Extreme-Ti star", r"Lowest-[Fe/H] star"]
-    Xcoords = [-25, 15, 4.5, -12,  18, -31, 22, 26,-22.5, -14, -2, -25]
-    Ycoords = [5.5 ,-6,  -2, -4,   6,  0,   1.5, -.5, -7, -2, -6, 14]
-    fsize   = [20 , 16,  12, 12,  15,  13, 11, 11, 11, 11, 11, 11]
-    sym = ["o", "v", "^", ">", "<", "s", "*", "<", "D", "h", "d", "H", "v", "p", "8", "H", "p"]
-    al  = [.6, .8, .8, .8, .8, .6, 1,1,1,1,1,1,1,1,1,1,1,1]
-    lw  = [0,.5,.5,.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
-    size= [9,12,12,12,12,15,18,18,18,18,18,18,18,18,18,18,18]
-    col = ["k", "m", "hotpink", "crimson", "r", "g", "orange", "gold",
-          "yellow", "yellow", "yellow", "yellow", "green", "royalblue", "royalblue",
-           "lime", "m"]
-elif sets=="errlim":
-    Xt = data["X_tsne_teffcut40_errlim_mc"]
-    Yt = data["Y_tsne_teffcut40_errlim_mc"]
-    classcol= np.char.rstrip(data["tSNE_class_errlim_mc"],' ')
-    subsets = ["thin", "thick1", "thick2", "thick3", "thick4",
-               "mpthin", "youngthin", "smr", "t4trans",
-               "debris1", "debris2", "debris3", "debris4", "debris5?", 
-               "t2trans1", "t2trans2", "highTi","t2trans3", "smr2", "lowMg"]
-    names   = ["Thin Disc", "Thick Disc I", "Thick Disc II", "Thick Disc III",
-               "Thick Disc IV", "Metal-poor \n Thin Disc", "Young", "SMR", "Transition",
-               "", "", "Satellite \n debris", "", "", r"TII/III", "", 
-               r"Extreme-Ti star", r""]
-    Xcoords = [-25, 15, 4.5, -12,  18, -31, 22, 26,-22.5, -14, -2, -25]
-    Ycoords = [5.5 ,-6,  -2, -4,   6,  0,   1.5, -.5, -7, -2, -6, 14]
-    fsize   = [20 , 16,  12, 12,  15,  13, 11, 11, 11, 11, 11, 11]
-    sym = ["o", "v", "^", ">", "<", "s", "o", "*", "<", "D", "h", "d", "H", "v",
-           "p", "8", "H", "p", "*", "s"]
-    al  = [.6, .8, .8, .8, .8, .6, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-    lw  = [0,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5]
-    size= [9,12,12,12,12,15,18,22,18,18,18,18,18,18,22,22,22,22,28,20]
-    col = ["k", "m", "hotpink", "crimson", "r", "g", "lightgrey", "orange", "gold",
-          "yellow", "yellow", "yellow", "yellow", "green", "royalblue", "royalblue",
-           "lime", "green", "gold", "k"]
-elif sets=="plain":
-    Xt = data["X_tsne_teffcut40"]
-    Yt = data["Y_tsne_teffcut40"]
-    classcol= np.char.rstrip(data["tSNE_class"],' ')
-    subsets = ["thin", "thick1", "thick2", "thick3",
-               "mpthin", "smr",
-               "t1trans", "debris", "highAlMg",
-               "t3trans", "highTioutlier","lowalphaoutlier"]
-    names   = ["Thin disc", "Thick Disc I", "Thick Disc II", "Thick Disc III",
-               "Metal-poor \n thin disc", "SMR", "Transition I",
-               "Satellite \n debris", r"High-[Al/Mg]", "Transition III",
-               r"Extreme-Ti star", r"Low-[Mg/Fe] star"]
-    Xcoords = [-25, 15, 4.5, -12,  18, -31, 22, 26,-22.5, -14, -2, -25]
-    Ycoords = [5.5 ,-6,  -2, -4,   6,  0,   1.5, -.5, -7, -2, -6, 14]
-    fsize   = [20 , 16,  12, 12,  15,  13, 11, 11, 11, 11, 11, 11]
-    sym = ["o", "v", "^", ">", "s", "*", "<", "D", "p", "8", "H", "h"]
-    al  = [.6, .6, .8, .8, .75, 1,1,1,1,1,1,1]
-    lw  = [0,0,.5,.5, .5, .5, .5, .5, .5, .5, .5, .5]
-    size= [6,9,9,9,12,20,18,18,18,18,22,25]
-    col = ["k", "r", "orange", "gold", "g", "orange",
-          "brown", "yellow", "royalblue", "hotpink", 
-          "lime", "black"]
-else:
-    raise ValueError("No valid 'subsets' name set")
+exinds = [ [0,2], [1,2], [1,6], [8,9], [7,9],
+           [0,1], None,  None,  None,  [7,8],
+           [0,3], None,  None,  None, [14,15],
+           [0,5], None,  None,  None, [12,13],
+           [0,16], [5,8],        [10,11]
+           [2,3],  ,  , ]
 
 #------------------------------------------------------------
 # Plot the results
 import matplotlib.gridspec as gridspec
 
+g   = plt.figure(figsize=(12, 14))
+
 #################
-# t-SNE plot apart
+# abundance plots around
 #################
-g   = plt.figure(figsize=(7, 9))
+gs = gridspec.GridSpec(6, 5)
+gs.update(left=0.05, bottom=0.06, right=0.98, top=0.98,
+           wspace=0.44, hspace=0.05)
+
+for jj in range(3):
+    ax = plt.Subplot(g, gs[0, jj])
+    g.add_subplot(ax)
+    for kk in np.arange(len(t.subsets)):
+        mask = (t.classcol == t.subsets[kk])
+        """ax.errorbar(xx[exinds[jj][0]][mask], xx[exinds[jj][1]][mask],
+                    xerr=xerr[exinds[jj][0]][mask], yerr=xerr[exinds[jj][1]][mask],
+                   ms=0, mec="k", capthick=0, elinewidth=1,
+                   mfc=t.col[kk], alpha=t.al[kk]/3., ecolor=t.col[kk], lw=0,
+                   marker=t.sym[kk], zorder=0)"""
+        ax.scatter(xx[exinds[jj][0]][mask], xx[exinds[jj][1]][mask],
+                   s=t.size[kk], lw=t.lw[kk], edgecolors="k",
+                   c=t.col[kk], alpha=t.al[kk],
+                   marker=t.sym[kk])
+    ax.set_xlabel(labels[exinds[jj][0]], fontsize=12)
+    ax.set_ylabel(labels[exinds[jj][1]], fontsize=12)
+
+#################
+# t-SNE plot in the center
+#################
 gs0 = gridspec.GridSpec(1, 1)
-gs0.update(left=0.1, bottom=0.05, right=0.98, top=0.7)
+gs0.update(left=0.25, bottom=0.45, right=0.78, top=0.78)
 ax  = plt.Subplot(g, gs0[0, 0])
 g.add_subplot(ax)
 if sets != "plain":
@@ -118,44 +94,55 @@ if sets != "plain":
     mcres =  np.genfromtxt("../tsne_results/harps_tsne_results_withteffcutmc5040_rand0.csv", delimiter=',',
                       dtype=[('Name', "|S14"), ('X', float), ('Y', float)])
     ax.scatter(mcres["X"], mcres["Y"], s=4, lw=0, c="grey", alpha=0.15)
-for kk in np.arange(len(subsets)):
-    mask = (classcol == subsets[kk])
-    if subsets[kk] == "debris1" and sets=="errlim":
-        ax.plot(Xt[mask], 9.7, marker=r"$\uparrow$", zorder=0, ms=20)
-        ax.scatter(Xt[mask], 9.5, s=4*size[kk], lw=lw[kk], edgecolors="k",
-                   c=col[kk], alpha=al[kk], marker=sym[kk])
+for kk in np.arange(len(t.subsets)):
+    mask = (t.classcol == t.subsets[kk])
+    if t.subsets[kk] == "debris1" and sets=="errlim":
+        ax.plot(t.Xt[mask], 9.7, marker=r"$\uparrow$", zorder=0, ms=20)
+        ax.scatter(t.Xt[mask], 9.5, s=4*t.size[kk], lw=t.lw[kk], edgecolors="k",
+                   c=t.col[kk], alpha=t.al[kk], marker=t.sym[kk])
     else:
-        ax.scatter(Xt[mask], Yt[mask], s=4*size[kk], lw=lw[kk], edgecolors="k",
-               c=col[kk], alpha=al[kk], marker=sym[kk])
+        ax.scatter(t.Xt[mask], t.Yt[mask], s=4*t.size[kk], lw=t.lw[kk], edgecolors="k",
+               c=t.col[kk], alpha=t.al[kk], marker=t.sym[kk])
     # Annotate population names
-    #ax.text(Xcoords[kk], Ycoords[kk], names[kk], fontsize=1.25*fsize[kk])
+    if kk < len(t.names):
+        ax.text(t.Xcoords[kk], t.Ycoords[kk], t.names[kk], fontsize=1.25*t.fsize[kk])
 ax.set_xlabel("t-SNE X dimension", fontsize=13)
 ax.set_ylabel("t-SNE Y dimension", fontsize=13)
-#ax.set_yscale("symlog")
 ax.axis([-13, 25, -8, 10.2])
-#ax.text(-10, 9, r"What happens when we impose $\sigma_{\rm [Fe/H]}, \sigma_{\rm [X/Fe]}> 0.03$", fontsize=15)
+
+plt.savefig("../im/harps_tsne-bigplot_"+sets+".png", dpi=200)  
+
+
+
+
+"""
+gs0 = gridspec.GridSpec(1, 1)
+gs0.update(left=0.1, bottom=0.05, right=0.98, top=0.7)
+ax  = plt.Subplot(g, gs0[0, 0])
+g.add_subplot(ax)
+
+...
+
 gs = gridspec.GridSpec(1, 3)
 gs.update(left=0.1, bottom=0.77, right=0.98, top=0.98,
            wspace=0.44, hspace=0.05)
 exinds = [ [1,2], [4,5], [2,3] ]
-
 for jj in range(3):
     ax = plt.Subplot(g, gs[0, jj])
     g.add_subplot(ax)
-    for kk in np.arange(len(subsets)):
-        mask = (classcol == subsets[kk])
+    for kk in np.arange(len(t.subsets)):
+        mask = (t.classcol == t.subsets[kk])
         ax.errorbar(xx[exinds[jj][0]][mask], xx[exinds[jj][1]][mask],
                     xerr=xerr[exinds[jj][0]][mask], yerr=xerr[exinds[jj][1]][mask],
                    ms=0, mec="k", capthick=0, elinewidth=1,
-                   mfc=col[kk], alpha=al[kk]/3., ecolor=col[kk], lw=0,
-                   marker=sym[kk], zorder=0)
+                   mfc=t.col[kk], alpha=t.al[kk]/3., ecolor=t.col[kk], lw=0,
+                   marker=t.sym[kk], zorder=0)
         ax.scatter(xx[exinds[jj][0]][mask], xx[exinds[jj][1]][mask],
-                   s=size[kk], lw=lw[kk], edgecolors="k",
-                   c=col[kk], alpha=al[kk],
-                   marker=sym[kk])
+                   s=t.size[kk], lw=t.lw[kk], edgecolors="k",
+                   c=t.col[kk], alpha=t.al[kk],
+                   marker=t.sym[kk])
     ax.set_xlabel(labels[exinds[jj][0]], fontsize=12)
     ax.set_ylabel(labels[exinds[jj][1]], fontsize=12)
-plt.savefig("../im/harps_tsne-plot_test-errlim.png", dpi=200)  
 
 #------------------------------------------------------------
 # Plot the results
@@ -236,10 +223,10 @@ for ii in range(2):
             if ii==jj:
                 ax.set_xlabel(lab[1-ii], fontsize=13)
                 ax.set_ylabel(lab[2-ii], fontsize=13)
-                """if ii==0:
-                    ax.set_xlim([4,13])
-                else:
-                    ax.set_ylim([4,13])"""
+                #if ii==0:
+                #    ax.set_xlim([4,13])
+                #else:
+                #    ax.set_ylim([4,13])
             else:
                 ax.xaxis.set_major_formatter(plt.NullFormatter())
                 ax.yaxis.set_major_formatter(plt.NullFormatter())
@@ -281,3 +268,4 @@ for ii in range(n_dim):
             pass
 
 plt.savefig("../im/harps_tsne-summary-plot_" + sets + ".png", dpi=200)  
+"""
--- python/open_data.py
+++ python/open_data.py
@@ -8,19 +8,86 @@ import numpy as np
 from astropy.io import fits as pyfits
 
 class harps(object):
-    def __init__():
+    def __init__(self):
         """
         Open the file of Delgado-Mena et al. (2017) and return a recarray
         """
-        hdu = pyfits.open(
-            '/home/friedel/Astro/Spectro/HARPS/DelgadoMena2017.fits',
-            names=True)
+        hdu = pyfits.open('../data/DelgadoMena2017.fits', names=True)
         self.data = hdu[1].data
-        return self.data
+        return None #self.data
 
-    def get_tsne_subsets( mc = True ):
+    def get_tsne_subsets(self, sets = "errlim" ):
         """
         Get the names and indices of the t-sne-defined subsets
         """
+        if sets=="mc":
+            self.Xt = self.data["X_tsne_teffcut40_mc"]
+            self.Yt = self.data["Y_tsne_teffcut40_mc"]
+            self.classcol= np.char.rstrip(self.data["tSNE_class_mc"],' ')
+            self.subsets = ["thin", "thick1", "thick2", "thick3", "thick4",
+                       "mpthin", "smr", "t4trans",
+                       "debris1", "debris2", "debris3", "debris4", "debris5?", 
+                       "t2trans1", "t2trans2", "highTi","thicklow"]
+            self.names   = ["Thin Disc", "Thick Disc I", "Thick Disc II", "Thick Disc III",
+                       "Thick Disc IV", "Metal-poor \n Thin Disc", "SMR", "Transition",
+                       "", "", "Satellite \n debris", "", "", r"TII/III", "", 
+                       r"Extreme-Ti star", r"Lowest-[Fe/H] star"]
+            self.Xcoords = [-25, 15, 4.5, -12,  18, -31, 22, 26,-22.5, -14, -2, -25]
+            self.Ycoords = [5.5 ,-6,  -2, -4,   6,  0,   1.5, -.5, -7, -2, -6, 14]
+            self.fsize   = [20 , 16,  12, 12,  15,  13, 11, 11, 11, 11, 11, 11]
+            self.sym = ["o", "v", "^", ">", "<", "s", "*", "<", "D", "h", "d", "H", "v", "p", "8", "H", "p"]
+            self.al  = [.6, .8, .8, .8, .8, .6, 1,1,1,1,1,1,1,1,1,1,1,1]
+            self.lw  = [0,.5,.5,.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
+            self.size= [9,12,12,12,12,15,18,18,18,18,18,18,18,18,18,18,18]
+            self.col = ["k", "m", "hotpink", "crimson", "r", "g", "orange", "gold",
+                  "yellow", "yellow", "yellow", "yellow", "green", "royalblue", "royalblue",
+                   "lime", "m"]
+        elif sets=="errlim":
+            self.Xt = self.data["X_tsne_teffcut40_errlim_mc"]
+            self.Yt = self.data["Y_tsne_teffcut40_errlim_mc"]
+            self.classcol= np.char.rstrip(self.data["tSNE_class_errlim_mc"],' ')
+            self.subsets = ["thin", "thick1", "thick2", "thick3", "thick4",
+                       "mpthin", "youngthin", "smr", "t4trans",
+                       "debris1", "debris2", "debris3", "debris4", "debris5?", 
+                       "t2trans1", "t2trans2", "highTi","t2trans3", "smr2", "lowMg"]
+            self.names   = ["Thin Disc", "Thick Disc", "Metal-rich Thick Disc",
+                       "Metal-poor \n Thin Disc", "Youngest Thin Disc", "SMR",
+                       "Transition", "Debris"]
+            self.Xcoords = [2,  14, -8, 7, -8, -12, -12.2, 18]
+            self.Ycoords = [5.5,-7.3, -6.3,.5, 7.5,  1,  -4.3,  7]
+            self.fsize   = [20 ,16, 12,12,  12, 13,  11, 15]
+            self.sym = ["o", "v", "^", ">", "<", "s", "o", "*", "<", "D", "h", "d", "H", "v",
+                   "p", "8", "H", "p", "*", "s"]
+            self.al  = [.6, .8, .8, .8, .8, .6, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+            self.lw  = [0,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5]
+            self.size= [9,12,12,12,12,15,18,22,18,18,18,18,18,18,22,22,22,22,28,20]
+            self.col = ["k", "m", "hotpink", "crimson", "r", "g", "lightgrey", "orange", "gold",
+                  "yellow", "yellow", "yellow", "yellow", "green", "royalblue", "royalblue",
+                   "lime", "green", "gold", "k"]
+        elif sets=="plain":
+            self.Xt = self.data["X_tsne_teffcut40"]
+            self.Yt = self.data["Y_tsne_teffcut40"]
+            self.classcol= np.char.rstrip(self.data["tSNE_class"],' ')
+            self.subsets = ["thin", "thick1", "thick2", "thick3",
+                       "mpthin", "smr",
+                       "t1trans", "debris", "highAlMg",
+                       "t3trans", "highTioutlier","lowalphaoutlier"]
+            self.names   = ["Thin disc", "Thick Disc I", "Thick Disc II", "Thick Disc III",
+                       "Metal-poor \n thin disc", "SMR", "Transition I",
+                       "Satellite \n debris", r"High-[Al/Mg]", "Transition III",
+                       r"Extreme-Ti star", r"Low-[Mg/Fe] star"]
+            self.Xcoords = [-25, 15, 4.5, -12,  18, -31, 22, 26,-22.5, -14, -2, -25]
+            self.Ycoords = [5.5 ,-6,  -2, -4,   6,  0,   1.5, -.5, -7, -2, -6, 14]
+            self.fsize   = [20 , 16,  12, 12,  15,  13, 11, 11, 11, 11, 11, 11]
+            self.sym = ["o", "v", "^", ">", "s", "*", "<", "D", "p", "8", "H", "h"]
+            self.al  = [.6, .6, .8, .8, .75, 1,1,1,1,1,1,1]
+            self.lw  = [0,0,.5,.5, .5, .5, .5, .5, .5, .5, .5, .5]
+            self.size= [6,9,9,9,12,20,18,18,18,18,22,25]
+            self.col = ["k", "r", "orange", "gold", "g", "orange",
+                  "brown", "yellow", "royalblue", "hotpink", 
+                  "lime", "black"]
+        else:
+            raise ValueError("No valid 'subsets' name set")
+
         
 
